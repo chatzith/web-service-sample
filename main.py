60185113bc2a0535c685a4cd7c270de57a475a5c
Christoforos Chatzitheodorou <christoforos.chatitheodorou@gmail.com>
2026-02-15 12:56:52 +0200

    feat: Added post method

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ A simple web-service for testing purposes.
 """
 
 # pylint: disable="import-error"
+# pylint: disable="too-few-public-methods"
+
+from datetime import datetime, timedelta
 
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from current_date import get_date
+
+
+class UserData(BaseModel):
+    """
+    HTTP Post data validation.
+    """
+
+    username: str
+    role: str
+
 
 app = FastAPI()
 
@@ -29,6 +43,23 @@ async def get_greetings(option: str, user: str | None = None):
         return {"message": f"Hello, {user}!"}
 
     return {"message": get_date()}
+
+
+@app.post("/user/")
+async def get_user(data: UserData) -> dict:
+    """
+    Returns the user access permission data.
+
+    :param data: username and role
+    :type data: dict
+    """
+
+    item_dict = data.model_dump()
+    item_dict["permission"] = True
+    item_dict["expires"] = (datetime.now() + timedelta(hours=1)).strftime(
+        "%Y-%m-%d %H:%M"
+    )
+    return {**item_dict}
 
 
 if __name__ == "__main__":
